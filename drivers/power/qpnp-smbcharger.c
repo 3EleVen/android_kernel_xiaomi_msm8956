8dcde2a292e1d23936d18a4c3cfeee897365e67e
umang96 <umangleekha3@gmail.com>
2016-08-07 13:48:22 +0530

    Max charge rate 2500mA

--- drivers/power/qpnp-smbcharger.c
+++ drivers/power/qpnp-smbcharger.c
@@ -402,7 +402,7 @@ module_param_named(
 	int, S_IRUSR | S_IWUSR
 );
 
-static int smbchg_default_dcp_icl_ma = 3000;
+static int smbchg_default_dcp_icl_ma = 2500;
 module_param_named(
 	default_dcp_icl_ma, smbchg_default_dcp_icl_ma,
 	int, S_IRUSR | S_IWUSR
